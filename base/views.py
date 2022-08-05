29f3ea0768ed43c3068358b8dcea026c23646e1c
Sami Nafis <sami.nafis2428@gmail.com>
2022-08-05 18:25:47 +0600

    Added Activites page

--- base/views.py
+++ base/views.py
@@ -199,3 +199,10 @@ def topicsPage(request):
     topics = Topic.objects.filter(name__icontains=q)
     context = {'topics':topics}
     return render(request, 'base/topics.html', context)
+
+def activitiesPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages':room_messages}
+    return render(request, 'base/activity.html', context)
+
+
